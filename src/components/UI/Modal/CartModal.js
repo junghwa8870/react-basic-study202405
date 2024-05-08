e040ecd55780b0aaa9ed9f38df70b7a12b98c1b5
junghwa <suejunghwa@gmail.com>
2024-05-08 15:40:07 +0900

    Cart Context와 Reducer로 전역상태관리 등등

--- src/components/UI/Modal/CartModal.js
+++ src/components/UI/Modal/CartModal.js
@@ -2,8 +2,10 @@ import React from 'react';
 import styles from './CartModal.module.scss';
 import Portal from '../Portal/Portal';
 
-const BackDrop = () => {
-  return <div className={styles.backdrop} />;
+const BackDrop = (onClose) => {
+  return (
+    <div className={styles.backdrop} onClick={onClose} />
+  );
 };
 
 const ModalOverlay = ({ children }) => {
@@ -14,7 +16,7 @@ const ModalOverlay = ({ children }) => {
   );
 };
 
-const CartModal = ({ children }) => {
+const CartModal = ({ children, onClose }) => {
   return (
     <>
       <Portal destId='backdrop-root'>
